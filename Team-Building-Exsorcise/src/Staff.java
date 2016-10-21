f924e24841c8ee18263787594885e8831c78a969
hector <hector@localhost.localdomain>
2016-10-20 23:38:53 -0500

    Completed Stadd

--- Team-Building-Exsorcise/src/Staff.java
+++ Team-Building-Exsorcise/src/Staff.java
@@ -1,4 +1,35 @@
 
-public class Staff {
+public class Staff extends Employee {
+	private String title;
 
+	/**
+	 * @param name
+	 * @param address
+	 * @param phoneNumber
+	 * @param email
+	 * @param officeNumber
+	 * @param salary
+	 * @param rank
+	 */
+	public Staff(String name, String address, String phoneNumber, String email, String officeNumber, double salary,
+			String rank) {
+		super(name, address, phoneNumber, email, officeNumber, salary);
+		this.title = rank;
+	}
+
+	public String getRank() {
+		return title;
+	}
+
+	public void setRank(String rank) {
+		this.title = rank;
+	}
+
+	@Override
+	public String toString() {
+		return "CLASS Staff [getName()=" + getName() + "]";
+	}
+	
+	
+	
 }
